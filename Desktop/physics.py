4757576fb414a77e4edac24c1afb5e89c562223d
Orrick Pun <opun50@stuy.edu>
2025-05-28 20:33:16 -0400

    Added more stuff

--- Desktop/physics.py
+++ Desktop/physics.py
@@ -4,22 +4,44 @@ scene = canvas(width = 600, height = 600)
 
 
 
-def cl():
-    return sphere(pos = vector(0,0,0), radius = 2)
-def h2o():
-    return sphere(pos = vector (100,100,0), radius = 3)
+def cl(x,y,z):
+    return sphere(pos = vector(x,y,z), radius = 79, color = color.green)
+def h2o(x,y,z):
+    oxy = sphere(pos = vector(x,y,z), radius = 48, color = color.red)
+    hydro1 = sphere(pos = vector(x+95.84*cos(radians(52.225)),y+95.84*sin(radians(52.225)),z), radius = 37)
+    hydro2 = sphere(pos = vector(x+95.84*cos(radians(52.225)),y-95.84*sin(radians(52.225)),z), radius = 37)
+    h20 = compoud([oxy, hydro1, hydro2])
+    return h2o(pos = vector (x,y,z))
+def xpos(evt):
+    return xpos = evt.value
+def ypos(evt):
+    return ypos = evt.value
+def zpos(evt):
+    return zpos = evt.value
+
+xposslider = slider(bind = xpos, min = 0, max = 1000, step = 1)
+yposslider = slider(bind = ypos, min = 0, max = 1000, step = 1)
+zposslider = slider(bind = zpos, min = 0, max = 1000, step = 1)
 
 def part1(evt):
     if evt.text is 'chlorine':
-        cl
+        cl(0,0,0)
     elif evt.text is 'water':
-        water
+        h2o(0,0,0)
+
+def part2(evt):
+    if evt.text is 'chlorine':
+        cl(xpos,ypos,zpos)
+    elif evt.text is 'water':
+        h2o(xpos,ypos,zpos)
     
 
 
-chlorine = radio(bind=part1,text='chlorine')
-water = radio(bind=part1, text='water')
+chlorine = radio(bind=part1,text='chlorine', name='part1')
+water = radio(bind=part1, text='water', name='part1')
 
+chlorine2 = radio(bind=part2,text='chlorine', name='part2')
+water2 = radio(bind=part2, text='water', name='part2')
 
 permittivity = 8.85e-12
 def fieldmag(particle):
