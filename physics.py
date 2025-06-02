18a26d349b59fa6fa0fc34fd9189c9cdc3ae298a
schuityan <opun50@stuy.edu>
2025-06-02 11:30:19 -0400

    Update physics.py

--- physics.py
+++ physics.py
@@ -3,8 +3,22 @@ Web VPython 3.2
 scene = canvas(width = 600, height = 600)
 
 e = 1.6e-19 #elementary charge
-def dipolemoment (q):
-    return q*
+k = 9e9 #Coulomb's law constant
+dt = 1000
+t = 0
+
+
+
+def dipolemoment(particle, positivecharge,negativecharge):
+    return particle.charge*(postivecharge.pos - negativecharge.pos)
+def torque(moment, field, theta):
+    return cross(moment,field)
+def force(particle1, particle2):
+    return (k*particle1.charge*particle2.charge)/(((particle1.pos-particle2.pos).mag)**2)
+    
+    
+    
+
 def cl(x,y,z):
     cl.charge = -e
     return sphere(pos = vector(x,y,z), radius = 79, color = color.green)
@@ -22,21 +36,21 @@ def ypos(evt):
 def zpos(evt):
     return zpos = evt.value
 
-xposslider = slider(bind = xpos, min = 0, max = 1000, step = 1)
-yposslider = slider(bind = ypos, min = 0, max = 1000, step = 1)
-zposslider = slider(bind = zpos, min = 0, max = 1000, step = 1)
+xposslider = slider(bind = xpos, min = 1, max = 1000, step = 1)
+yposslider = slider(bind = ypos, min = 1, max = 1000, step = 1)
+zposslider = slider(bind = zpos, min = 1, max = 1000, step = 1)
 
 def part1(evt):
     if evt.text is 'chlorine':
-        cl(0,0,0)
+        part1 = cl(0,0,0)
     elif evt.text is 'water':
-        h2o(0,0,0)
+        part1 = h2o(0,0,0)
 
 def part2(evt):
     if evt.text is 'chlorine':
-        cl(xpos,ypos,zpos)
+        part2 = cl(xpos,ypos,zpos)
     elif evt.text is 'water':
-        h2o(xpos,ypos,zpos)
+        part2 = h2o(xpos,ypos,zpos)
     
 
 
@@ -46,8 +60,17 @@ water = radio(bind=part1, text='water', name='part1')
 chlorine2 = radio(bind=part2,text='chlorine', name='part2')
 water2 = radio(bind=part2, text='water', name='part2')
 
-permittivity = 8.85e-12
+permittivity = 8.85e-12 
+
 def fieldmag(particle):
     rad = (particle.pos - point.pos)
     return ((1/(4*pi*permittivity*rad**2))*particle.charge)
+    
+while((part2.pos-part2.pos).mag>(part1.radius+part2.radius)):
+    rate(dt)
+    a = force(part1,part2)
+    v = a*dt
+    x = v*dt
+    part1.pos = part1.pos
+    t = t+dt
 
