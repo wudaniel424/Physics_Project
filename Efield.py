c4b401cc16bdd8192f86ea9c0e117c331f6b453e
wudaniel424 <144823289+wudaniel424@users.noreply.github.com>
2025-06-07 13:58:25 -0400

    Update Efield.py

--- Efield.py
+++ Efield.py
@@ -6,11 +6,13 @@ s=0.001
 qn=sphere(pos=vector(-s/2,0,0), radius=s/5, color=color.cyan)
 qp=sphere(pos=vector(s/2,0,0), radius=s/5, color=color.yellow)
 
+midpoint = (qn.pos + qp.pos) * 1/2
 qn.q=-q
 qp.q=q
 
 R=0.006
 theta=0
+
 
 N=16
 dtheta=2*pi/N
@@ -20,7 +22,7 @@ while R<0.01:
     
     while theta<2*pi:
   
-        ro=R*vector(cos(theta),sin(theta),0)
+        ro=R*vector(cos(theta),sin(theta),0) - midpoint
 
         rp=ro-qp.pos
         rn=ro-qn.pos
@@ -35,18 +37,6 @@ while R<0.01:
     theta=0
     R+=0.001
 
-while theta<2*pi:
-  
-  ro=R*vector(cos(theta),0,sin(theta))
 
-  rp=ro-qp.pos
-  rn=ro-qn.pos
-  Ep=k*qp.q*rp/mag(rp)**3
-
-  En=k*qn.q*rn/mag(rn)**3
-  E=Ep+En
-
-  Earrow=arrow(pos=ro, axis=Escale*E)
-  theta=theta+dtheta
  
 
